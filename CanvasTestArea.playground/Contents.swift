bc310120771d395491d33d2e6771e9ea4ba53ef7
Santos, Diego <dsantos@lcs-mba3217.lcs.on.ca>
2017-10-02 17:37:40 -0400

    the nose

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -33,14 +33,12 @@ canvas.drawEllipse(centreX: 230, centreY: 420, width: 100, height: 100)
 //Draw a eyes
 canvas.fillColor=Color.white
 canvas.drawEllipse(centreX: 150, centreY: 300, width: 270, height: 300)
-
-// Draw eyes
 canvas.fillColor=Color.black
 canvas.drawEllipse(centreX: 200, centreY: 350, width: 80, height: 90)
 canvas.drawEllipse(centreX: 100, centreY: 350, width: 80, height: 90)
 canvas.fillColor=Color.white
-canvas.drawEllipse(centreX: 185, centreY: 360, width: 25, height: 25)
-canvas.drawEllipse(centreX: 115, centreY: 360, width: 25, height: 25)
+canvas.drawEllipse(centreX: 115, centreY: 360, width: 20, height: 20)
+canvas.drawEllipse(centreX: 185, centreY: 360, width: 20, height: 20)
 
 // Draw cheeks
 canvas.fillColor = Color(hue: 0, saturation: 30, brightness: 100, alpha: 100)
@@ -51,8 +49,13 @@ canvas.drawEllipse(centreX: 200, centreY: 290, width: 50, height: 30)
 
 // Draw the nose
 canvas.fillColor=Color.black
-canvas.drawEllipse(centreX: 150, centreY: 250, width: 40, height: 25)
-
+canvas.drawEllipse(centreX: 135, centreY: 240, width: 40, height: 30)
+canvas.drawEllipse(centreX: 155, centreY: 240, width: 40, height: 30)
+canvas.fillColor=Color.white
+canvas.drawEllipse(centreX: 160, centreY: 255, width: 30, height: 50)
+canvas.drawEllipse(centreX: 130, centreY: 255 , width: 30, height: 50)
+canvas.fillColor=Color.black
+canvas.drawEllipse(centreX: 150, centreY: 263, width: 60, height: 20)
 
 // This code is necessary to see the result in the Assistant Editor at right
 PlaygroundPage.current.liveView = canvas.imageView
