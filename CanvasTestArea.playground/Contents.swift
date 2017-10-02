43383cf79441a37a03fd3cc0f6426d6809d372d6
Santos, Diego <dsantos@lcs-mba3217.lcs.on.ca>
2017-10-02 11:15:52 -0400

    Ears, eyes and part of the cheeks

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -23,15 +23,31 @@ import PlaygroundSupport
 // Create a new canvas
 let canvas = Canvas(width: 300, height: 500)
 
-// Draw a circle at the origin with radius of 50 pixels
-canvas.drawEllipse(centreX: 0, centreY: 0, width: 50, height: 50)
-
-// Show where the origin is
-canvas.drawAxes()
-
 // Draw an ellipse in a different color at the centre of the canvas
-canvas.fillColor = Color.green
-canvas.drawEllipse(centreX: 150, centreY: 250, width: 50, height: 100)
+canvas.fillColor = Color.black
+
+// Draw ears
+canvas.drawEllipse(centreX: 80, centreY: 420, width: 100, height: 100)
+canvas.drawEllipse(centreX: 230, centreY: 420, width: 100, height: 100)
+
+//Draw a eyes
+canvas.fillColor=Color.white
+canvas.drawEllipse(centreX: 150, centreY: 300, width: 270, height: 300)
+
+// Draw eyes
+canvas.fillColor=Color.black
+canvas.drawEllipse(centreX: 200, centreY: 350, width: 80, height: 90)
+canvas.drawEllipse(centreX: 100, centreY: 350, width: 80, height: 90)
+canvas.fillColor=Color.white
+canvas.drawEllipse(centreX: 185, centreY: 360, width: 25, height: 25)
+canvas.drawEllipse(centreX: 115, centreY: 360, width: 25, height: 25)
+
+// Draw cheeks
+canvas.fillColor = Color(hue: 0, saturation: 30, brightness: 100, alpha: 100)
+canvas.drawShapesWithBorders = false
+canvas.drawEllipse(centreX: 100, centreY: 290, width: 50, height: 30)
+canvas.drawEllipse(centreX: 100, centreY: 290, width: 50, height: 30)
+
 
 
 // This code is necessary to see the result in the Assistant Editor at right
